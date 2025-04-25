2679e64968bee59a49f456cc93b0397c7c3549bf
Yevhen <benatanlev@icloud.com>
2025-04-25 21:44:08 +0300

    Done

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,15 @@
-import s from "./FriendListItem.module.css"
+import s from "./FriendListItem.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={s.friendCard}>
-      <img src={avatar} alt={name} width="48" />
-      <p>{name}</p>
-      <p>{isOnline}</p>
+      <img className={s.friendAvatar} src={avatar} alt={name} width="48" />
+      <p className={s.friendName}>{name}</p>
+      <p className={isOnline ? s.online : s.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 };
 
 export default FriendListItem;
-
